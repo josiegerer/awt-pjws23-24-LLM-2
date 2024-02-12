1f33ef09d3d939ee5aa86c19fde6f1002e4d0760
kiuyenzo <nguyen.tanja@hotmail.com>
2024-02-12 12:28:29 +0100

    add html loader

--- vector_store/vector_creation.py
+++ vector_store/vector_creation.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, WebBaseLoader, UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
@@ -7,7 +7,21 @@ DB_FAISS_PATH = "./vector_store/faiss_store"
 DATA_DIR = "./vector_store/data"
 
 def vector_store_creation():
-    loader = DirectoryLoader(DATA_DIR, glob="*.pdf", loader_cls=PyPDFLoader)
+    html_loader = DirectoryLoader(DATA_DIR, glob="*.html", loader_cls=UnstructuredHTMLLoader)
+    pdf_loader = DirectoryLoader(DATA_DIR, glob="*.pdf", loader_cls=PyPDFLoader)
+
+    # web_loader = WebBaseLoader(["https://medium.com/@orangecollege/what-is-the-difference-between-general-and-academic-english-4890a698f578",
+    #                            "https://speakenglishalfresco.com/blog/informal-vs-formal-language/",
+    #                            "https://www.busuu.com/en/languages/informal-vs-formal",
+    #                            "https://languagetool.org/insights/post/formal-vs-informal-style/",
+    #                            "https://testmaximizer.com/formal-and-informal"])
+
+    all_loaders = [pdf_loader, html_loader]
+
+    documents = []
+    for loader in all_loaders:
+        documents.extend(loader.load())
+
     documents = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) 
