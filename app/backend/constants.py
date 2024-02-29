4454fca51d4690ac72e2924a586d49fcc110b92a
MertAkil <mert_27@hotmail.de>
2024-02-29 15:09:14 +0100

    Upload requirements.txt

--- app/backend/constants.py
+++ app/backend/constants.py
@@ -1,4 +1,4 @@
-DB_PATH = "mysql+pymysql://<username>:<password>@localhost/test_llm_app"
+DB_PATH = "mysql+pymysql://root:gamefan2714@localhost/test_llm_app"
 DB_FAISS_PATH = './../../vector_store/faiss_store'
 MODEL_PATH = "./../../../llama_weights/llama-2-7b-chat.Q4_K_M.gguf"
 MAX_TOKEN = 1024
